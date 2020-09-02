df89dad4c173815a70051f42bd45609ef4fc95ca
Sriya <tsriya6@gmail.com>
2020-09-02 21:38:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import foods from './food';
+import { choice, remove } from './helpers';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    //Randomly draw a fruit from the array
+    let fruit = choice(foods);
 
+    //Log the message "I'd like one RANDOMFRUIT, please."
+    console.log(`I'd like one ${fruit}, please.`);
+
+    //Log the message "Here you go : RANDOMFRUIT"
+    console.log(`Here you go : ${fruit}`);
+
+    //Log the message  "Delicious! May I have another?"
+    console.log(`Delicious! May I have another?`);
+
+    //remove the fruit from array of fruits
+    let remaining = remove(foods, fruit);
+    //Log the message "I'm sorry we're all out. We have FRUITSLeft left."
+    console.log(
+      `I'm sorry we're all out. We have ${foods.length + 1} other fruits left`
+    );
+    return (
+      <div className='container'>
+        {foods}
+        <p>I'd like one {fruit}, please.</p>
+        <p>Here you go : {fruit}</p>
+        <p>Delicious! May I have another?</p>
+        <p>
+          I'm sorry we're all sold out. We have other fruits left. Would you
+          want to try any of these {remaining}
+        </p>
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/helpers.js
@@ -0,0 +1,13 @@
+function choice(items) {
+  let randomIndex = Math.floor(Math.random() * items.length);
+  return items[randomIndex];
+}
+function remove(items, item) {
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] === item) {
+      return [...items.slice(0, i), items.slice(i + 1)];
+    }
+  }
+}
+
+export { choice, remove };
